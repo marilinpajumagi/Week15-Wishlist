a7b4cec79f245f96e5978e30d876e335a91a0042
Marilin Pajumägi <marilin.pajumagi@tktk.ee>
2024-04-12 14:42:55 +0300

    pictures

--- server.js
+++ server.js
@@ -30,3 +30,20 @@ const port = 5000;
 app.listen(port, () => {
     console.log('Server is running &{port}.');
 })
+
+const express = require('express');
+const path = require('path');
+
+const path = express();
+
+
+const imagesDirectory = path.join(__dirname, 'images');
+
+
+app.use('/images', express.static(imagesDirectory));
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- wish.js
+++ wish.js
@@ -8,8 +8,18 @@ module.exports = class Wish {
         this.desvription = wish;
     }
 
+    const fs = require('fs');
+const path = require('path');
+const filePath = path.join(path.dirname(require.main.filename), 'data', 'wishes.json');
+
+module.exports = class Wish {
+
+    constructor(wish){
+        this.description = wish; // corrected typo in "description"
+    }
+
     saveWish() {
-        fs.readFile(filePath, (error, fileContent) =>{
+        fs.readFile(filePath, (error, fileContent) => {
             let wishes = [];
 
             if(!error){
@@ -25,23 +35,21 @@ module.exports = class Wish {
                 if(!error) {
                     console.log('wish saved');
                 } else {
-                console.log(error);
+                    console.log(error);
                 }
             });
 
         });
+    }
     
     static fetchAllWishes(callBack){
-        fs.readFile(filePath, (error, fileContent) {
+        fs.readFile(filePath, (error, fileContent) => { // added missing arrow function
             if(error){
                 callBack([]);
-            };
-
-            callBack(JSON.parse(fileContent));
+            } else {
+                callBack(JSON.parse(fileContent));
+            }
         });
     }
-    
-    }
-
 
 }
